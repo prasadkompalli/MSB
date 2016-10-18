0f06b4e644ae0a9683f49b8ebfebecb53dc83c5c
prasadkompalli <prasadkompalli156@gmail.com>
2016-10-19 01:53:48 +0530

    first commit

--- MSBTeamImage/script/Xampp.sh
+++ MSBTeamImage/script/Xampp.sh
@@ -1,6 +1,5 @@
 su -c "yum install wget -y"
 su -c "sudo mkdir -p /xamppserversoftware/v7"
-#su -c "sudo cd /xamppserversoftware/v7"
 
 su -c "sudo mv xampp-linux-x64-7.0.9-1-installer.run /xamppserversoftware/v7"
 
@@ -9,7 +8,6 @@ su -c "wget https://www.apachefriends.org/xampp-files/7.0.9/xampp-linux-x64-7.0.
 su -c "yum install chmod -y"
 su -c "sudo chmod +x xampp-linux-x64-7.0.9-1-installer.run"
 su -c "./xampp-linux-x64-7.0.9-1-installer.run"
-#su -c "sudo /opt/lampp/lampp stop"
 
 
 
--- MSBTeamImage/script/softwares.sh
+++ MSBTeamImage/script/softwares.sh
@@ -2,14 +2,6 @@ su -c "sudo mkdir -p /opt/java8_MSB/jdk8_MSB"
 su -c "yum install wget -y"
 su -c "yum install echo -y"
 su -c "sudo yum install source -y"
-#su -c "sudo cd /opt/java8_MSB/jdk8_MSB/"
-
-
-#su -c "sudo alternatives --install /usr/bin/java java /usr/java/jdk1.8.0_102/bin/java 2"
-#su -c "sudo alternatives --install /usr/bin/jar jar /usr/java/jdk1.8.0_102/bin/jar 2"
-#su -c "sudo alternatives --install /usr/bin/javac javac /usr/java/jdk1.8.0_102/bin/javac 2"
-#su -c "sudo alternatives --set jar /usr/java/jdk1.8.0_102/bin/jar"
-#su -c "sudo alternatives --set javac /usr/java/jdk1.8.0_102/bin/javac"
 
 
 
@@ -22,10 +14,6 @@ su -c "sudo alternatives --install /usr/bin/jar jar /opt/java8_MSB/jdk8_MSB/jdk1
 su -c "sudo alternatives --install /usr/bin/javac javac /opt/java8_MSB/jdk8_MSB/jdk1.8.0_102/bin/javac 2"
 su -c "sudo alternatives --set jar /opt/java8_MSB/jdk8_MSB/jdk1.8.0_102/bin/jar"
 su -c "sudo alternatives --set javac /opt/jdk1.8.0_102/bin/javac"
-
-#export JRE_HOME=/usr/local/jdk1.8.0_102/jre
-#export PATH=$PATH:./usr/local/jdk1.8.0_102/bin:/usr/local/jdk1.8.0_102/jre:$HOME/bin
-#export CLASSPATH=/usr/local/jdk1.8.0_102/lib
 
 
 
@@ -49,11 +37,6 @@ su -c "sudo source ~/.bash_profile"
 
 
 
-#su -c "sudo echo 'export JAVA_HOME=/usr/java/jdk1.8.0_102'  >> /etc/profile"
-#su -c "sudo echo 'export JRE_HOME=/usr/java/jdk1.8.0_102/jre' >> /etc/profile"
-#su -c "sudo echo 'export PATH=$PATH:/usr/java/jdk1.8.0_102/bin:./usr/java/jdk1.8.0_102/jre:/usr/apache/apache-tomcat-8.5.4/bin:$HOME/bin'  >> /etc/profile"
-#su -c "sudo echo 'export CATALINA_HOME=/usr/apache/apache-tomcat-8.5.4'  >> /etc/profile"
-
 su -c "sudo mkdir -p /apache/softwares/apache8"
 su -c "yum install wget -y"
 su -c "sudo wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.4/bin/apache-tomcat-8.5.4.tar.gz"
@@ -69,9 +52,6 @@ su -c "sudo chmod -R 775 ./apache-tomcat-8.5.4.tar.gz/bin/*.sh"
 su -c "sudo chmod +x ./apache/softwares/apache8/apache-tomcat-8.5.4/lib/.jar"
 su -c "sudo chmod -R 775 ./apache-tomcat-8.5.4.tar.gz/conf/*.xml"
 
-#su -c "sudo chmod -R 775 /balu/tomcat/apache-tomcat-8.5.4/conf/*.xml"
-#su -c "sudo chmod +x /balu/tomcat/apache-tomcat-8.5.4//lib/"
-
 
 su -c "sudo groupadd tomcat"
 su -c "sudo useradd miracle -g tomcat -s /bin/bash"
@@ -84,9 +64,9 @@ su -c "sudo systemctl restart firewalld"
 su -c "echo 'export JAVA_HOME=/opt/java8MSB/jdk8_MSB/jdk1.8.0_102'  >> /.bash_profile"
 su -c "echo 'export CLASSPATH=./opt/java8MSB/jdk8_MSB/jdk1.8.0_102/lib'  >> /.bash_profile"
 su -c "echo 'export CLASSPATH=$CLASSPATH:./apache/softwares/apache8/apache-tomcat-8.5.4/lib'  >> /.bash_profile"
-#su -c "echo 'export CLASSPATH=$CLASSPATH:./apache/softwares/apache8/apache-tomcat-8.5.4/lib'  >> ./apache/softwares/apache8/apache-tomcat-8.5.4/bin/setclasspath.sh"
+
 su -c "echo 'export CATALINA_HOME=./apache/softwares/apache8/apache-tomcat-8.5.4'  >> /.bash_profile"
-#su -c "echo 'export CATALINA_BASE=./apache/softwares/apache8/apache-tomcat-8.5.4/webapps' >> /.bash_profile"
+
 su -c "sudo echo 'export PATH=$PATH:./opt/java8MSB/jdk8_MSB/jdk1.8.0_102/bin:./opt/java8MSB/jdk8_MSB/jdk1.8.0_102/jre:./apache/softwares/apache8/apache-tomcat-8.5.4/bin/:$HOME/bin' >> ~/.bash_profile"
 su -c "sudo ~/.bash_profile"
 
@@ -94,7 +74,7 @@ su -c "sudo ~/.bash_profile"
 su -c " echo 'export JAVA_HOME=./opt/java8MSB/jdk8_MSB/jdk1.8.0_102'  >> ~/.bashrc"
 su -c " echo 'export CLASSPATH=./opt/java8MSB/jdk8_MSB/jdk1.8.0_102/lib'  >> ~/.bashrc"
 su -c "echo 'export  CLASSPATH=$CLASSPATH:./apache/softwares/apache8/apache-tomcat-8.5.4/lib'  >> ~/.bashrc"
-#su -c "echo 'export CLASSPATH=$CLASSPATH:./apache/softwares/apache8/apache-tomcat-8.5.4/lib'  >> ./apache/softwares/apache8/apache-tomcat-8.5.4/bin/setclasspath.sh"
+
 su -c " echo 'export CATALINA_HOME=./apache/softwares/apache8/apache-tomcat-8.5.4'  >> /.bashrc"
 su -c "sudo echo 'export PATH=$PATH:./opt/java8MSB/jdk8_MSB/jdk1.8.0_102/bin:./opt/java8MSB/jdk8_MSB/jdk1.8.0_102/jre:./apache/softwares/apache8/apache-tomcat-8.5.4/bin:$HOME/bin' >> ~/.bashrc"
 su -c "sudo ~/.bashrc" 
@@ -107,14 +87,9 @@ su -c "sudo echo 'export PATH=$PATH:./opt/java8MSB/jdk8_MSB/jdk1.8.0_102/bin:./o
 su -c "sudo /etc/profile" 
 su -c "unset CATALINA_HOME"
 
-#su -c "sed 's/port=\"8080\"/port=\"1122\"/g' /balu/tomcat/apache-tomcat-8.5.4/conf/server.xml"
+
 
 su -c "sed 's/port=\"8080\"/port=\"8085\"/g' /apache/softwares/apache8/apache-tomcat-8.5.4/conf/server.xml"
-#su -c "sed -i '/<\/tomcat-users>/ i\<user username=\"msb\" password=\"msb\" roles=\"admin,manager-gui,manager-script,manager-status\"/>' >>  ./apache/softwares/apache8/apache-tomcat-8.5.4/conf/tomcat-users.xml"
-
-#su -c "sudo /apache/softwares/apache8/apache-tomcat-8.5.4/bin/shutdown.sh" 
-
-#su -c "/apache/softwares/apache8/apache-tomcat-8.5.4/bin/startup.sh" 
 
 
   
